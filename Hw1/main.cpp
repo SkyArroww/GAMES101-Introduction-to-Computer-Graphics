9e94fa6d814613c8890afd636170e92e61ccaf3b
SkyArroww <1536513344@qq.com>
2024-01-15 20:33:03 +0800

    Update Clang-format

--- Hw1/main.cpp
+++ Hw1/main.cpp
@@ -6,8 +6,7 @@
 
 constexpr double MY_PI = 3.1415926;
 
-Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
-{
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
@@ -18,8 +17,7 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle)
-{
+Eigen::Matrix4f get_model_matrix(float rotation_angle) {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
     // TODO: Implement this function
@@ -30,8 +28,7 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     return model;
 }
 
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
-{
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar) {
     // Students will implement this function
 
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
@@ -56,29 +53,26 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float z
     return projection;
 }
 
-int main(int argc, const char **argv)
-{
+int main(int argc, const char** argv) {
     float angle = 0;
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3)
-    {
+    if (argc >= 3) {
         command_line = true;
         angle = std::stof(argv[2]); // -r by default
-        if (argc == 4)
-        {
+        if (argc == 4) {
             filename = std::string(argv[3]);
         }
     }
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f eye_pos = { 0, 0, 5 };
 
-    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+    std::vector<Eigen::Vector3f> pos { { 2, 0, -2 }, { 0, 2, -2 }, { -2, 0, -2 } };
 
-    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+    std::vector<Eigen::Vector3i> ind { { 0, 1, 2 } };
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
@@ -86,8 +80,7 @@ int main(int argc, const char **argv)
     int key = 0;
     int frame_count = 0;
 
-    if (command_line)
-    {
+    if (command_line) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -103,8 +96,7 @@ int main(int argc, const char **argv)
         return 0;
     }
 
-    while (key != 27)
-    {
+    while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -120,12 +112,9 @@ int main(int argc, const char **argv)
 
         std::cout << "frame count: " << frame_count++ << '\n';
 
-        if (key == 'a')
-        {
+        if (key == 'a') {
             angle += 10;
-        }
-        else if (key == 'd')
-        {
+        } else if (key == 'd') {
             angle -= 10;
         }
     }
